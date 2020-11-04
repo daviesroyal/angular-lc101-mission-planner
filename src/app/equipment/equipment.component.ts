ba9058af4b930df73512598653c637683bb63f63
Alex Davies <ro.morningstar@gmail.com>
2020-11-03 18:22:47 -0600

    all but the bonus

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  itemBeingEdited: string = null;
+  equipment: string[] = ["Habitat dome", "Drones", "Food containers", "Oxygen tanks"];
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(item: string) {
+    this.equipment.push(item);
+  }
+
+  remove(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  save(updatedItem: string, item: string) {
+    item = updatedItem;
+    this.itemBeingEdited = null;
+  }
+  
 }
